037ccf874a0f6710f7335dac9720f12756a10f00
jwh62 <jwh62@duke.edu>
2021-09-10 12:27:06 -0400

    fix

--- 028_tests_power/test-power.c
+++ 028_tests_power/test-power.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,12 +16,12 @@ void performTest() {
   run_check(1, 2, 1);
   run_check(2, 2, 4);
   run_check(2, 2.0, 4.0);
-  run_check(-2, 2, 4);
-  run_check(1, -1, 1);
+  //run_check(-2, 2, 4);
+  //run_check(1, -1, 1);
   run_check(2, 10, 1024);
   run_check(100, 2, 10000);
-  run_check(-1, 2, 1);
-  run_check(-1, 3, -1);
+  //run_check(-1, 2, 1);
+  //  run_check(-1, 3, -1);
   run_check(2, 20, 1048576);
   run_check(1, 5, 1);
   run_check(0, 0, 1);
