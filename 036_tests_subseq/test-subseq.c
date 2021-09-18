2898483a7e2f3db0e4c4bc0ddf2c531dc091e671
jwh62 <jwh62@duke.edu>
2021-09-17 23:01:22 -0400

    fix

--- 036_tests_subseq/test-subseq.c
+++ 036_tests_subseq/test-subseq.c
@@ -7,6 +7,7 @@ void run_check(int * array, size_t n, size_t expected_ans) {
   size_t ans = maxSeq(array, n);
 
   if (ans != expected_ans) {
+    printf("failed test. expected: %ld, actual: %ld", expected_ans, ans);
     exit(EXIT_FAILURE);
   }
 }
@@ -15,31 +16,36 @@ void performTest() {
   int arr[] = {1, 2, 3};
   int * p = arr;
   run_check(p, 3, 3);
+  printf("test 1 correct");
 
   int arr2[] = {1, 1, 2};
   p = arr2;
   run_check(p, 3, 2);
-
+  printf("test 2 correct");
+  
   int arr3[] = {4, 3, 1, 2, 0, 1, 2, 3};
   p = arr3;
   run_check(p, 8, 4);
-
+  printf("test 3 correct");
+  
   int arr4[] = {1, 2, 1, 3, 5, 7, 2, 4, 6, 9};
   p = arr4;
   run_check(p, 10, 4);
-
+  printf("test 4 correct");
   int arr5[3] = {0};
   p = arr5;
   run_check(p, 3, 1);
+  printf("test 5 correct");
 
   int arr6[] = {-5, -4, -3, 0, 2, 3, 4, 5};
   p = arr6;
   run_check(p, 8, 8);
-
+  printf("test 6 correct");
+  
   int arr7[] = {5, 4, 3, 2, 1};
   p = arr7;
   run_check(p, 5, 1);
-
+  printf("test 7 correct");
   int arr8[] = {1, 2, 3, 3, 3, 3};
   p = arr8;
   run_check(p, 6, 3);
