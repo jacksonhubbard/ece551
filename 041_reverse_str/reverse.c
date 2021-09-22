09338899fef464e334aef937f4e2a4f13c2bfcf1
jwh62 <jwh62@duke.edu>
2021-09-21 20:48:25 -0400

    ans

--- 041_reverse_str/reverse.c
+++ 041_reverse_str/reverse.c
@@ -4,32 +4,38 @@
 
 void reverse(char * str) {
   //WRITE ME!
-  if ((!(str[0] == '\n')) && (str != NULL)) {
-    size_t len = strlen(str);
-    char * i = str;
-    char * j = i + len - 1;
+  if (str == NULL) {
+    return;
+  }
+  else {
+    if (!(str[0] == '\n')) {
+      size_t len = strlen(str);
+      char * i = str;
+      char * j = i + len - 1;
 
-    while (i < j) {
-      char temp = *i;
-      *i = *j;
-      *j = temp;
-      i++;
-      j--;
+      while (i < j) {
+        char temp = *i;
+        *i = *j;
+        *j = temp;
+        i++;
+        j--;
+      }
     }
   }
 }
 
 int main(void) {
-  char str0[] = "";
-  char str1[] = "123";
-  char str2[] = "abcd";
-  char str3[] = "Captain's log, Stardate 42523.7";
-  char str4[] = "Hello, my name is Inigo Montoya.";
-  char str5[] = "You can be my wingman anyday!";
-  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be "
-                "no tomorrow!";
-  char * array[] = {str0, str1, str2, str3, str4, str5, str6};
-  for (int i = 0; i < 7; i++) {
+  //char str0[] = "";
+  //char str1[] = "123";
+  //char str2[] = "abcd";
+  //char str3[] = "Captain's log, Stardate 42523.7";
+  //char str4[] = "Hello, my name is Inigo Montoya.";
+  //char str5[] = "You can be my wingman anyday!";
+  //char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be "
+  //"no tomorrow!";
+  //  char * array[] = {NULL, str1, str2, str3, str4, str5, str6};
+  char * array[] = {NULL};
+  for (int i = 0; i < 1; i++) {
     reverse(array[i]);
     printf("%s\n", array[i]);
   }
