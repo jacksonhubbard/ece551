d98a08fdfc840b2a51b58fb269a459a9e7205ad7
jwh62 <jwh62@duke.edu>
2021-09-21 20:53:26 -0400

    ans

--- 041_reverse_str/reverse.c
+++ 041_reverse_str/reverse.c
@@ -25,16 +25,16 @@ void reverse(char * str) {
 }
 
 int main(void) {
-  //char str0[] = "";
-  //char str1[] = "123";
-  //char str2[] = "abcd";
-  //char str3[] = "Captain's log, Stardate 42523.7";
-  //char str4[] = "Hello, my name is Inigo Montoya.";
-  //char str5[] = "You can be my wingman anyday!";
-  //char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be "
-  //"no tomorrow!";
-  //  char * array[] = {NULL, str1, str2, str3, str4, str5, str6};
-  char * array[] = {NULL};
+  char str0[] = "";
+  char str1[] = "123";
+  char str2[] = "abcd";
+  char str3[] = "Captain's log, Stardate 42523.7";
+  char str4[] = "Hello, my name is Inigo Montoya.";
+  char str5[] = "You can be my wingman anyday!";
+  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be "
+                "no tomorrow!";
+  char * array[] = {str0, str1, str2, str3, str4, str5, str6};
+
   for (int i = 0; i < 1; i++) {
     reverse(array[i]);
     printf("%s\n", array[i]);
