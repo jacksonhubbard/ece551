79b229f00810f4d825663be2a977f574dc9705bc
jwh62 <jwh62@duke.edu>
2021-10-08 23:51:57 -0400

    ans

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,23 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  size_t size = strlen(inputName);
+
+  char * ans = malloc(size * sizeof(char));
+
+  for (size_t i = 0; i < size; i++) {
+    ans[i] = inputName[i];
+  }
+
+  char * test = ".counts";
+  for (size_t j = size; j < size + 6; j++) {
+    ans[j] = test[j - size];
+  }
+
+  return ans;
 }
