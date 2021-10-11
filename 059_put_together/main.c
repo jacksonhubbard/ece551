82adb63484a9dcd7e6def558ee20df1d272b6b30
jwh62 <jwh62@duke.edu>
2021-10-11 14:12:59 -0400

    ans

--- 059_put_together/main.c
+++ 059_put_together/main.c
@@ -1,38 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "kv.h"
+
 #include "counts.h"
+#include "kv.h"
 #include "outname.h"
 
 counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
   //WRITE ME
-  return NULL;
+  counts_t * countsStruct = createCounts();  // has an countsArr ** arr and size
+
+  // read line by line and call addCount
+  for (size_t i = 0; i < kvPairs->len; i++) {
+    addCount(countsStruct, kvPairs->arr[i].value);
+  }
+  printCounts(countsStruct, stdout);
+
+  return countsStruct;
 }
 
 int main(int argc, char ** argv) {
   //WRITE ME (plus add appropriate error checking!)
- //read the key/value pairs from the file named by argv[1] (call the result kv)
+  if (argc <= 2) {
+    fprintf(stderr, "Usage: key/values inputFileName(s)\n");
+    return EXIT_FAILURE;
+  }
 
- //count from 2 to argc (call the number you count i)
+  //read the key/value pairs from the file named by argv[1] (call the result kv)
+  kvarray_t * kv = readKVs(argv[1]);
 
+  //count from 2 to argc (call the number you count i)
+  for (int i = 2; i < argc; i++) {
     //count the values that appear in the file named by argv[i], using kv as the key/value pair
     //   (call this result c)
+    counts_t * c = countFile(argv[i], kv);
 
     //compute the output file name from argv[i] (call this outName)
-
+    char * outName = computeOutputFileName(argv[i]);
 
     //open the file named by outName (call that f)
-
+    FILE * f = fopen(outName, "W");
     //print the counts from c into the FILE f
+    printCounts(c, f);
 
     //close f
+    fclose(f);
 
     //free the memory for outName and c
-
-
-
- //free the memory for kv
+    free(outName);
+    free(c);
+    printf("%s", "new inputfile\n");
+  }
+  //free the memory for kv
+  freeKVs(kv);
 
   return EXIT_SUCCESS;
 }
