354101157f7ae8715fd994ade6819902663c9c46
jwh62 <jwh62@duke.edu>
2021-10-18 16:01:17 -0400

    ans

--- 070_circle/circle.cpp
+++ 070_circle/circle.cpp
@@ -4,22 +4,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+using namespace std;
+
 void Circle::move(double dx, double dy) {
   this->p.move(dx, dy);
 }
 double Circle::intersectionArea(const Circle & otherCircle) {
-  double d = abs(p.getX() - otherCircle.p.getX());
+  double r1 = this->r;
+  double r2 = otherCircle.r;
 
-  double arg1 = this->r * this->r *
-                acos((d * d + this->r * this->r - otherCircle.r * otherCircle.r) /
-                     (2 * d * this->r));
-  double arg2 = otherCircle.r * otherCircle.r *
-                acos((d * d - this->r * this->r + otherCircle.r * otherCircle.r) /
-                     (2 * d * otherCircle.r));
-  double arg3 =
-      0.5 * sqrt((d + this->r - otherCircle.r) * (d - this->r + otherCircle.r) *
-                 ((-1 * d) + this->r + otherCircle.r) * (d + this->r + otherCircle.r));
-  double ans = arg1 + arg2 - arg3;
+  double x1 = p.getX();
+  double x2 = otherCircle.p.getX();
 
-  return ans;
+  double y1 = p.getY();
+  double y2 = otherCircle.p.getY();
+
+  double area1 = r1 * r1 * 3.1415926535;
+  double area2 = r2 * r2 * 3.1415926535;
+
+  double d = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+  if (d > (r1 + r2)) {
+    return 0.0;
+  }
+
+  if (d <= abs(r1 - r2) && r1 >= r2) {
+    return area2;
+  }
+  else if (d <= abs(r1 - r2) && r1 < r2) {
+    return area1;
+  }
+  else {
+    double arg1 = this->r * this->r *
+                  acos((d * d + this->r * this->r - otherCircle.r * otherCircle.r) /
+                       (2 * d * this->r));
+    double arg2 = otherCircle.r * otherCircle.r *
+                  acos((d * d - this->r * this->r + otherCircle.r * otherCircle.r) /
+                       (2 * d * otherCircle.r));
+    double arg3 =
+        0.5 * sqrt((d + this->r - otherCircle.r) * (d - this->r + otherCircle.r) *
+                   ((-1 * d) + this->r + otherCircle.r) * (d + this->r + otherCircle.r));
+    double ans = arg1 + arg2 - arg3;
+
+    return ans;
+  }
 }
