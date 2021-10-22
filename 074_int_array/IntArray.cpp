c740e148078954c335bb55667fe03ea93c83f4b0
jwh62 <jwh62@duke.edu>
2021-10-22 11:23:57 -0400

    ans

--- 074_int_array/IntArray.cpp
+++ 074_int_array/IntArray.cpp
@@ -1,37 +1,82 @@
 #include "IntArray.h"
+
 #include <assert.h>
+
 #include <ostream>
 
 IntArray::IntArray() {
-
+  numElements = 0;
+  data = NULL;
 }
 IntArray::IntArray(int n) {
-
+  numElements = n;
+  int * dataTemp = new int[n];
+  data = dataTemp;
 }
 
-IntArray::IntArray(const IntArray & rhs)  {
-
+IntArray::IntArray(const IntArray & rhs) {
+  data = new int[rhs.numElements];
+  for (int i = 0; i < rhs.numElements; i++) {
+    data[i] = rhs.data[i];
+  }
+  numElements = rhs.numElements;
 }
+
 IntArray::~IntArray() {
-
+  delete[] data;
 }
 
 IntArray & IntArray::operator=(const IntArray & rhs) {
+  data = rhs.data;
+  numElements = rhs.numElements;
+  return *this;
 }
+
 const int & IntArray::operator[](int index) const {
+  assert(index < numElements);
+  assert(index >= 0);
+  return data[index];
 }
+
 int & IntArray::operator[](int index) {
+  assert(index < numElements);
+  assert(index >= 0);
+  return data[index];
 }
 
 int IntArray::size() const {
+  return numElements;
 }
 
 bool IntArray::operator==(const IntArray & rhs) const {
+  if (numElements == rhs.numElements) {
+    for (int i = 0; i < numElements; i++) {
+      if (data[i] != rhs.data[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
 }
 
 bool IntArray::operator!=(const IntArray & rhs) const {
+  if (numElements == rhs.numElements) {
+    for (int i = 0; i < numElements; i++) {
+      if (data[i] != rhs.data[i]) {
+        return true;
+      }
+    }
+    return false;
+  }
+  return true;
 }
 
 std::ostream & operator<<(std::ostream & s, const IntArray & rhs) {
-
+  s << "{";
+  for (int i = 0; i < rhs.size() - 1; i++) {
+    s << rhs[i] << ", ";
+  }
+  s << rhs[rhs.size() - 1] << "}";
+  return s;
 }
