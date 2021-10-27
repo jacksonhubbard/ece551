6167c2d7cb633a1869a34c9190ae77675b940d9b
jwh62 <jwh62@duke.edu>
2021-10-27 17:18:39 -0400

    ans

---
+++ 079_sort_cpp/sortLines.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>  // std::sort
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void noInputs() {
+  string line;
+  vector<string> strings;
+
+  while (std::getline(std::cin, line)) {
+    strings.push_back(line);
+  }
+
+  std::sort(strings.begin(), strings.end());
+  for (std::vector<string>::iterator it = strings.begin(); it != strings.end(); ++it)
+    std::cout << *it << "\n";
+}
+
+void inputs(char * filename) {
+  string line;
+  vector<string> strings;
+
+  ifstream file(filename);
+  if (file.is_open()) {
+    while (getline(file, line)) {
+      //     cout << line << '\n';
+      strings.push_back(line);
+    }
+    file.close();
+  }
+  std::sort(strings.begin(), strings.end());
+  for (std::vector<string>::iterator it = strings.begin(); it != strings.end(); ++it)
+    std::cout << *it << "\n";
+}
+
+int main(int argc, char ** argv) {
+  //WRITE YOUR CODE HERE!
+
+  if (argc == 1) {
+    noInputs();
+  }
+  else {
+    for (int i = 1; i < argc; i++) {
+      inputs(argv[i]);
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
