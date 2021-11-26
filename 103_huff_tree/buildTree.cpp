68358610a60968203e93c4e77e674190b66ea49b
jwh62 <jwh62@duke.edu>
2021-11-26 09:44:39 -0500

    ans

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,26 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t pq;
+
+  for (int i = 0; i < 257; i++) {
+    if (counts[i] != 0) {
+      pq.push(new Node(i, counts[i]));
+    }
+  }
+
+  while (pq.size() > 1) {
+    Node * node1 = pq.top();
+    pq.pop();
+    Node * node2 = pq.top();
+    pq.pop();
+
+    Node * newNode = new Node(node1, node2);
+    pq.push(newNode);
+  }
+
+  Node * root = pq.top();
+  pq.pop();
+  return root;
+}
